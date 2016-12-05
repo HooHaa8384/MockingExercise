5fc8c9f9f3b7704003e4e90f85e3bd8f63fb3c09
Joshua Slominski <joshua.slominski@gm.com>
2016-12-05 15:31:29 -0500

    Getting bolier plate to work

---
+++ app/src/test/java/com/example/gzh13q/mockingexample/LastPlayerOnTeamTest.java
@@ -0,0 +1,15 @@
+package com.example.gzh13q.mockingexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gzh13q on 12/5/2016.
+ */
+public class LastPlayerOnTeamTest {
+    @Test
+    public void Dummytest(){
+        assertTrue(true);
+    }
+}
